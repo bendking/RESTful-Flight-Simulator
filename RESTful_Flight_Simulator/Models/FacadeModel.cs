e52e43f868456f7958933232dfcbcd54601496fe
Ofec Israel <ofec2000@gmail.com>
2019-05-31 00:12:48 +0300

    facade's skeleton and lon lat object

--- RESTful_Flight_Simulator/Models/FacadeModel.cs
+++ RESTful_Flight_Simulator/Models/FacadeModel.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using RESTful_Flight_Simulator.Models;
 namespace RESTful_Flight_Simulator.Models
 {
     public class FacadeModel
     {
-        public FacadeModel()
+
+        private static readonly FacadeModel _singleton = new FacadeModel();
+
+        public static FacadeModel GetInstance()
         {
+            return _singleton;
+        }
+
+        private FacadeModel()
+        {
+
+        }
+
+        public LonLat GetCurrentLonLat (string ip, int port)
+        {
+            return new LonLat(50, 50);
+        }
+
+        public LonLat GetCurrentLonLatAndSave(string ip, int port, string file)
+        {
+            LonLat x = GetCurrentLonLat(ip, port);
+            // save x
+            return x;
+        }
+
+        public LonLat GetNextLonLatFromFile(string file, int index)
+        {
+            return new LonLat(50, 50);
         }
     }
 }
--- RESTful_Flight_Simulator/Models/LonLat.cs
+++ RESTful_Flight_Simulator/Models/LonLat.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.Text;
+
 namespace RESTful_Flight_Simulator.Models
 {
     public class LonLat
     {
-        public LonLat()
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+
+        public LonLat(double _Lon, double _Lat)
         {
+            Lon = _Lon;
+            Lat = _Lat;
         }
+
+        public string ToXml()
+        {
+            //Initiate XML stuff
+            StringBuilder sb = new StringBuilder();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            XmlWriter writer = XmlWriter.Create(sb, settings);
+
+            writer.WriteStartDocument();
+            writer.WriteStartElement("Coordinates");
+
+            writer.WriteElementString("Lon", this.Lon.ToString());
+            writer.WriteElementString("Lat", this.Lat.ToString());
+
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
+            return sb.ToString();
+        }
+
     }
 }
