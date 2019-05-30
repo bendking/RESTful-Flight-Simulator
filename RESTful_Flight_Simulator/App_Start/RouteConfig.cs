dd72ccbe43708084380a58e97c1675ddcc4aa10d
Ofec Israel <ofec2000@gmail.com>
2019-05-30 16:56:34 +0300

    fixed routing

--- RESTful_Flight_Simulator/App_Start/RouteConfig.cs
+++ RESTful_Flight_Simulator/App_Start/RouteConfig.cs
@@ -12,59 +12,49 @@ namespace RESTful_Flight_Simulator
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
+
             //
             // Dynamic routes
             //
-
-            // Display route of plane as it flies (update per interval (in seconds))
-            routes.MapRoute(
-                name: "Route",
-                url: "{controller}/{ip}/{port}/{interval}",
-                defaults: new { controller = "display", action = "DisplayLiveRoute", ip = "127.0.0.1", port = 5400, interval = 1 }
-            );
-
-            // Determine whether to route to DisplayLocation or DisplaySavedRoute
-            routes.MapRoute(
-                name: "Display",
-                url: "{controller}/{param1}/{param2}/",
-                defaults: new { controller = "display", action = "DetermineRoute" }
-            );
 
             // Save route of plane as it flies into a file
             routes.MapRoute(
                 name: "Save",
                 url: "{controller}/{ip}/{port}/{interval}/{duration}/{flight_id}",
-                defaults: new { controller = "save", action = "SaveRoute", ip = "127.0.0.1", port = 5400, interval = 1, duration = 10, flight_id = "flight1" }
-            );
-
-
-            //
-            // Hardcoded routes
-            //
-
-            /*
-            // Display current location of plane
-            routes.MapRoute(
-                name: "Dot",
-                url: "{controller}/127.0.0.1/5400",
-                defaults: new { controller = "display", action = "DisplayLocation" }
-            );
-
-            // Load saved route from memory and animate it (4 updates per second)
-            routes.MapRoute(
-                    name: "Load",
-                    url: "{controller}/flight1/4",
-                    defaults: new { controller = "display", action = "DisplaySavedRoute" }
-            );
-
-            // Save route of plane as it flies into a file
-            routes.MapRoute(
-                name: "Save",
-                url: "{controller}/127.0.0.1/5400/4/10/flight1",
                 defaults: new { controller = "save", action = "SaveRoute" }
             );
-            */
+
+            // Display route of plane as it flies (update per interval (in seconds))
+            routes.MapRoute(
+                "Route",
+                "{controller}/{ip}/{port}/{interval}",
+                new { controller = "display", action = "DisplayLiveRoute"},
+                new {ip = @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"}
+            );
+
+            // DisplayLocation
+            routes.MapRoute(
+                "Display",
+                "{controller}/{ip}/{port}/",
+                new { controller = "display", action = "DisplayLocation"},
+                new { ip = @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$" }
+            );
+
+            // DisplaySavedRoute
+            routes.MapRoute(
+                name: "DisplayAndSave",
+                url: "{controller}/{file}/{interval}/",
+                defaults: new { controller = "display", action = "DisplaySavedRoute"}
+            );
+
+            // for debug 
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{ip}/{port}",
+               defaults: new { controller = "display", action = "DisplayLocation", ip = "127.0.0.1", port = 5400 }
+           );
+
+
         }
     }
 }
