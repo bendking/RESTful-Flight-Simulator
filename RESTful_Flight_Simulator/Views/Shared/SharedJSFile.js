f547140925ed36f93a77ee2e6523d747b303c164
Ofec Israel <ofec2000@gmail.com>
2019-05-30 14:08:08 +0300

    put the draw function in shared file

---
+++ RESTful_Flight_Simulator/Views/Shared/SharedJSFile.js
@@ -0,0 +1,10 @@
+﻿function drawCircle(x,y,canvasName) {
+    var canvas = document.getElementById(canvasName);
+        var context = canvas.getContext("2d");
+        context.arc(x, y, 3, 0, 2 * Math.PI, false);
+        context.fillStyle = "#FF0000";
+    context.fill();
+    context.lineWidth = 1;
+context.strokeStyle = '#000000';
+context.stroke();
+}
