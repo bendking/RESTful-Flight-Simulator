3d0b77e45039e8293dc0507aff931c824c920dce
ofeci <ofec2000@gmail.com>
2019-06-02 19:21:41 +0100

    fix

---
+++ RESTful_Flight_Simulator/Content/JavaScript.js
@@ -0,0 +1,18 @@
+﻿function drawCircle(x, y, canvasName) {
+
+    var canvas = document.getElementById(canvasName);
+    x = (x + 180.0) * canvas.width / 360.0
+    y = (y + 90) * canvas.height / 180.0
+    var context = canvas.getContext("2d");
+    context.arc(x, y, 1, 0, 2 * Math.PI, false);
+    context.fillStyle = "#FF0000";
+    context.fill();
+
+    /*
+ * for borders
+    context.lineWidth = 0.3;
+    context.strokeStyle = '#000000';
+    context.stroke();
+    */
+}
+
